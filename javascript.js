dfac8a276cc44eb5a029566e9481e5ec7784abc3
abushahid81 <abushahidsardar81@gmail.com>
2024-02-16 18:39:03 +0530

    javascript practic

---
+++ javascript.js
@@ -0,0 +1,208 @@
+// let a = "abu";
+// console.log(a);
+
+// let b = 10;
+// console.log(b);
+
+// function  fun1() {
+//     let num = 5;
+//     let num2 = 8;
+//     // return num*num2;
+//     console.log();
+// }
+// let x = fun1(5,8);///passing .arguments to functions 
+// console.log(x);
+// let y = fun1(6,9);
+// console.log(y);
+
+// //paraneters of functions.
+// function  fun1(num,num2){
+
+
+// return num*num2;
+ 
+
+// }
+
+// let fullname = "tony stark"
+// typeof(fullname )
+
+// (1)Artimatic Operators
+// let a = 5;
+// let b = 12;
+
+// console.log("a = ", a, "& b = ", b);
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a % b = ", a % b); // modules
+// console.log("a ** b = ", a ** b);// 5^2 five to the power 2 = 5 x 5 = 25 this is called Exponentiation Operator.
+
+// // (2)Unary Operator
+// let a = 10;
+// let b = 12;
+
+// console.log("a + b = ", a + b); 
+
+// a = a + 1;// 11
+
+// console.log("a = ", a)
+
+// // Short cut
+// a++;//12
+
+// console.log("a = ", a)
+
+//(3)Asignment Operator
+
+// let a = 5;
+// let b = 10;
+
+// // 1 assign +=
+// a += 5;
+
+// console.log("a = ", a);
+
+// // 2 assign -=
+// a -= 5;
+
+// console.log("a = ", a);
+
+// // 3 assign *=
+// a *= 5;
+
+// console.log("a = ", a);
+
+// // 4 assign **=
+// a **= 5;
+
+// console.log("a = ", a);
+
+// // 5 assign /=
+// a /= 5;
+
+// console.log("a = ", a);
+
+// // 6 assign %=
+// a %= 5;
+
+// console.log("a = ", a);
+
+// (4) Comparison Operators
+
+//equal to ==
+// let a = 10;
+// let b = 20;
+
+// console.log("a == b" ,a == b);//false
+
+// // equal to ==
+// let c = 10;
+// let d = 10;
+
+// console.log("c == d" ,c == d);//true
+
+// // equal to & type ===
+// let e = 10;
+// let f = 20;
+
+// console.log("e === f" ,e === f);//flase
+
+// // equal to & type ===
+// let g = 20;
+// let h = 20;
+
+// console.log("g === h" ,g === h);//true
+
+// // not equal to !=
+// let i = 20;
+// let j = 20;
+
+// console.log("i != j" ,i != j);//false
+
+// // not equal to !=
+// let k = 10;
+// let l = 20;
+
+// console.log("k != l" ,k != l);//true
+
+// //not equal to & type !==
+// let m = 10;
+// let n = 20;
+
+// console.log("m !== n" ,m !== n);//true
+
+// //not equal to & type !==
+// let o = 10;
+// let p = 20;
+
+// console.log("o !== p" ,o !== p);//true
+
+// //(4) Comparison Operators
+// let a = 10;
+// let bb = 20;
+
+// console.log("10 > 20" ,a > bb);//false
+
+// let ab = 10;
+// let bc = 20;
+
+// console.log("10 < 20" ,ab < bc);//true
+// let mo = 10;
+// let no = 20;
+
+// console.log("10 >= 20" ,mo >= no);//false
+// let op = 10;
+// let pm = 20;
+
+// console.log("10 <= 20" ,op <= pm);//true
+
+// Logical Operator
+// && Operator
+// let a = 10;
+// let b = 20;
+
+// let abu1 = a > b;//false 
+// let abu2 = a === 10;//true /not equal to & type
+// console.log("abu1 && abu2= ", abu1 && abu2)//false
+// //&& Operator
+// let c = 10;
+// let d = 20;
+
+// let abu3 = a < b; //true
+// let abu4 = a === 10;//true /not equal to & type
+// console.log("abu3 && abu4 = ", abu3 && abu4)//true
+// // OR Opreator
+// let f = 10;
+// let g = 20;
+
+// let abu5 = a < b; //true
+// let abu6 = a === 10;//true /not equal to & type
+// console.log("abu5 || abu6 = ", abu5 || abu6)//true
+
+// // OR Operator
+// let h = 10;
+// let i = 20;
+
+// let abu7 = a > b; //false
+// let abu8 = a === 20;//false /not equal to & type
+// console.log("abu7 || abu8 =", abu7 || abu8)//false
+
+// //! NOT false into true and true into false.
+// let j = 10;
+// let k = 20;
+
+// let abu10 = k === 10;//true /not equal to & type
+// console.log("abu9 ! abu10 =", !(abu9 , abu10))//true
+// let abu9 = j >
+ k; //true
+
+// //! NOT false into true and true into false.
+// let l = 10;
+// let m = 20;
+
+// let abu11 = l < m; //false
+// let abu12 = m === 20;//false /not equal to & type
+// console.log("abu11 ! abu12 =", !(abu11 , abu12))//false
+
+let x = 100 + 50 * 3;
+console.log(x);
